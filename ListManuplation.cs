c003a3158d2fb6831e291a0e396adcebadfb29c6
pranavimadala <34127558+pranavimadala@users.noreply.github.com>
2019-10-10 12:25:35 +0530

    Add files via upload

--- ListManuplation.cs
+++ ListManuplation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security;
@@ -9,86 +10,100 @@ namespace CreateLists
 {
     class ListManuplation
     {
+        public  string SPurl { get; private set; }
+        public  string UserName { get; private set; }
+        public  ClientContext ClientContext { get; set; }
+        public  Web web { get; set; }
+        public  List newList { get; set; }
+        public  string title { get; set; }
+        public  int choice { get; set; }
+        public  SecureString password { get; set; }
+        public string description { get; set; }
+        public int choicee { get; set; }
+        public string name { get; set; }
+        public string displayName { get; set; }
+
         public void Init()
         {
-            Details details = new Details();
+            
+
             Service service = new Service();
             UI ui = new UI();
             Strings strings = new Strings();
             
             ui.ShowMessage(strings.url);
-            details.SPurl = ui.ReadMessage();
+            SPurl = ui.ReadMessage();
             ui.ShowMessage(strings.username);
-            details.UserName = ui.ReadMessage();
+           UserName = ui.ReadMessage();
             ui.ShowMessage(strings.password);
-            details.password = service.GetPassword();
+            password = service.GetPassword();
 
-            service.Login(details.ClientContext, details.UserName, details.password,details.web,details.SPurl);
+            service.Login(ClientContext, UserName, password,web,SPurl);
             do
             {
                 ui.ShowMessage(strings.mainMenu);
-                details.choice = ui.GetChoice();
+                choice = ui.GetChoice();
                 ui.ShowMessage(strings.listTitle);
-                details.title = ui.ReadMessage();
-                switch (details.choice)
+                title = ui.ReadMessage();
+                switch (choice)
                 {
                     case 1:
                         
                         ui.ShowMessage(strings.listDescription);
-                        details.description = ui.ReadMessage();
-                        details.title=service.CreateList(details.ClientContext,details.newList,details.title,details.description);
-                        ui.ShowMessage(strings.success + details.title);
+                        description = ui.ReadMessage();
+                        title=service.CreateList(ClientContext,newList,title,description);
+                        ui.ShowMessage(strings.success + title);
                         break;
                     case 2:
                         ui.ShowMessage(strings.fieldsMenu);
-                        details.choicee = ui.GetChoice();
+                        choicee = ui.GetChoice();
                         ui.ShowMessage(strings.fieldName);
-                        details.name = ui.ReadMessage();
+                        name = ui.ReadMessage();
                         ui.ShowMessage(strings.displayName);
-                        details.displayName = ui.ReadMessage();
-                        service.AddFields(details.title,details.newList, details.ClientContext,details.choicee,details.name,details.displayName);
+                        displayName = ui.ReadMessage();
+                        service.AddFields(title,newList, ClientContext,choicee,name,displayName);
                         break;
 
                     case 3:
-                        service.BreakListPermissions(details.ClientContext,details.newList,details.title);
+                        service.BreakListPermissions(ClientContext,newList,title);
                         break;
                     case 4:
-                        service.BreakItemPermissions(details.ClientContext, details.newList, details.title);
+                        service.BreakItemPermissions(ClientContext, newList, title);
                         break;
                     case 5:
                         ui.ShowMessage(strings.addListPermissions);
-                        details.choicee = ui.GetChoice();
+                        choicee = ui.GetChoice();
                         ui.ShowMessage(strings.listTitle);
-                        details.title = ui.ReadMessage();
-                       if(details.choicee==1)
+                        title = ui.ReadMessage();
+                       if(choicee==1)
                             ui.ShowMessage(strings.username);
                        else
                             ui.ShowMessage(strings.groupTitle);
-                        details.name = ui.ReadMessage();
-                        if (details.choicee == 1 || details.choicee == 2 || details.choicee == 3)
+                        name = ui.ReadMessage();
+                        if (choicee == 1 || choicee == 2 || choicee == 3)
                         {
-                            if (details.choicee == 1 || details.choicee == 2)
+                            if (choicee == 1 || choicee == 2)
                                 ui.ShowMessage(strings.perimissions);
                             else
                                 ui.ShowMessage(strings.groupDescription);
-                                details.description = ui.ReadMessage();
+                                description = ui.ReadMessage();
                         }
-                        if (details.choicee == 2)
+                        if (choicee == 2)
                             ui.ShowMessage(strings.group);
-                        service.AddListPermissions(details.title, details.newList, details.ClientContext, details.choicee,details.web,details.name,details.description,ui);
+                        service.AddListPermissions(title, newList, ClientContext, choicee,web,name,description,ui);
                         break;
                     case 6:
                         ui.ShowMessage(strings.mainMenu);
-                        details.choice = ui.GetChoice();
+                        choice = ui.GetChoice();
                         ui.ShowMessage(strings.listTitle);
-                        details.title = ui.ReadMessage();
+                        title = ui.ReadMessage();
                         ui.ShowMessage(strings.username);
-                        details.name = ui.ReadMessage();
+                        name = ui.ReadMessage();
                         ui.ShowMessage(strings.perimissions);
-                        details.description = ui.ReadMessage();
-                        if (details.choicee == 2)
+                        description = ui.ReadMessage();
+                        if (choicee == 2)
                             ui.ShowMessage(strings.group);
-                        service.AddItemPermissions(details.title, details.newList, details.ClientContext, details.choicee, details.web, details.name, details.description, ui);
+                        service.AddItemPermissions(title, newList, ClientContext, choicee, web, name, description, ui);
                         break;
                     case 7:
                         break;
@@ -96,7 +111,7 @@ namespace CreateLists
                         ui.ShowMessage(strings.wrongChoice);
                         break;
                 }
-            } while (details.choice != 7);
+            } while (choice != 7);
         }
     }
     
